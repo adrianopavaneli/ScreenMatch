7e6e5b77a70e9ac7b3f47cc5c146317fa0f7dd30
Adriano Pavaneli <adriano.pavaneli.dev@gmail.com>
2024-01-14 22:14:16 -0300

    Encapsulamento

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,10 +1,16 @@
 public class Filme {
-    String nome;
-    int anoDeLancamento;
-    boolean incluidoNoPlano;
-    double somaDasAvaliacoes;
-    int totalDeAvaliacoes;
-    int duracaoEmMinutos;
+   private String nome;
+   private int anoDeLancamento;
+   private boolean incluidoNoPlano;
+   private double somaDasAvaliacoes;
+   private int totalDeAvaliacoes;
+   private int duracaoEmMinutos;
+
+ 
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
 
     void exibeFichaTecnica(){
         System.out.println("Nome do filme: " + nome);
